0f9e429804d4c6ee4e8bfa6f34fa998f3c805acb
Tai Vo <tai.vo1812@huskers.unl.edu>
2021-02-09 02:01:44 -0600

    First commit

---
+++ script.js
@@ -0,0 +1,28 @@
+const daysEl = document.getElementById("days");
+const hoursEl = document.getElementById("hours");
+const minsEl = document.getElementById("mins");
+const secondsEl = document.getElementById("seconds");
+
+const myBirthday = "7 Jan 2022";
+
+function countdown() {
+    const myBirthdayDate = new Date(myBirthday);
+    const currentDate = new Date();
+
+    const totalSeconds = (myBirthdayDate - currentDate) / 1000;
+
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const mins = Math.floor(totalSeconds / 60) % 60;
+    const seconds = Math.floor(totalSeconds) % 60;
+
+    daysEl.innerHTML = days;
+    hoursEl.innerHTML = hours;
+    minsEl.innerHTML = mins;
+    secondsEl.innerHTML = seconds;
+}
+
+// initial call
+countdown();
+
+setInterval(countdown, 1000);
